f9e238932165e7a9a342cbd8ba814b05cc90a223
Suriya Subramanian <suriya@alumni.cs.utexas.edu>
2015-10-04 06:00:48 +0000

    Move XValidatedModel into models.py.

---
+++ xvalidate/models.py
@@ -0,0 +1,72 @@
+
+
+from django.db import models
+from django.utils.decorators import classonlymethod
+from django.core.exceptions import ValidationError
+from django.core import checks
+from .validators import AbnormalValues
+
+__all__ = (
+    'XValidatedModel',
+)
+
+
+class XValidatedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    class XVMeta:
+        spec = None
+
+    @classmethod
+    def _check_xvmeta(cls, **kwargs):
+        if not issubclass(cls.XVMeta, XValidatedModel.XVMeta):
+            yield checks.Error(
+                "Model's XVMeta should inherit from XValidatedModel.XVMeta",
+                hint="Use XVMeta(XValidatedModel.XVMeta) in the model's definition",
+                obj=cls,
+                id='xvalidate.E001',
+            )
+        for name in dir(cls.XVMeta):
+            if name.startswith('_'):
+                continue
+            if name not in dir(XValidatedModel.XVMeta):
+                yield checks.Error(
+                    "Unexpected field '{}' in XVMeta definition".format(name),
+                    hint="Check for typos in XVMeta",
+                    obj=cls,
+                    id='xvalidate.E002',
+                )
+
+    def _raise_validation_error_if_needed(self, result):
+        value = result['value']
+        if AbnormalValues.is_abnormal(value):
+            return
+        if not value:
+            raise ValidationError(result['message'])
+
+    def _clean_xvmeta(self):
+        spec = self.XVMeta.spec
+        if not spec:
+            return
+        if not isinstance(spec, (list, tuple)):
+            spec = [spec]
+        for s in spec:
+            result = s._clean(self)
+            self._raise_validation_error_if_needed(result)
+
+    @classonlymethod
+    def check(cls, **kwargs):
+        errors = super(XValidatedModel, cls).check(**kwargs)
+        errors.extend(cls._check_xvmeta(**kwargs))
+        spec = cls.XVMeta.spec
+        if spec:
+            if not isinstance(spec, (list, tuple)):
+                spec = [spec]
+            for s in spec:
+                errors.extend(s._check(cls))
+        return errors
+
+    def clean(self):
+        super(XValidatedModel, self).clean()
+        self._clean_xvmeta()
