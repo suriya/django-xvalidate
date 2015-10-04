7ea69f5fb3eb73726731341d4518395430d06848
Suriya Subramanian <suriya@alumni.cs.utexas.edu>
2015-10-04 17:37:10 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-xvalidate',
-    version='0.1.0',
+    version='0.2.0',
     packages=['xvalidate'],
     include_package_data=True,
     license='MIT License',
--- xvalidate/__init__.py
+++ xvalidate/__init__.py
@@ -1,3 +1,4 @@
 
+__version__ = '0.2.0'
 from .validators import *  # NOQA
 from .models import *  # NOQA
