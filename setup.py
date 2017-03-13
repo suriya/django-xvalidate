0e2fd6fd4414d676386dcab31bbf7d56ae823ff5
Suriya Subramanian <suriya@alumni.cs.utexas.edu>
2017-03-13 10:32:34 +0000

    Bump version to 0.4.2.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-xvalidate',
-    version='0.4.1',
+    version='0.4.2',
     packages=['xvalidate'],
     include_package_data=True,
     license='MIT License',
--- xvalidate/__init__.py
+++ xvalidate/__init__.py
@@ -1,4 +1,4 @@
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 from .validators import *  # NOQA
 from .models import *  # NOQA
